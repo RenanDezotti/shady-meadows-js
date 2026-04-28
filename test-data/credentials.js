865b96de93e4420b90c5611d49cc13c83fcff351
Renan QA <renandezotti10@gmail.com>
2026-04-28 11:54:58 +0100

    config: add test data and credentials structure

---
+++ test-data/credentials.js
@@ -0,0 +1,12 @@
+const ADMIN = {
+  username: "admin",
+  password: "password",
+  url: "/admin",
+};
+
+const INVALID_USER = {
+  username: "wronguser",
+  password: "wrongpassword",
+};
+
+module.exports = { ADMIN, INVALID_USER };
